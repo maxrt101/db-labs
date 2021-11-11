2d57b61ef7ef0c547897a98b9727c82c2dca6f0b
Maksym Tkachuk <s2m4f6@gmail.com>
2021-11-11 11:08:54 +0200

    Fixed `list` command. Updated artifact id

--- src/main/scala/com/maxrt/console/cmd/impl/ListCommand.scala
+++ src/main/scala/com/maxrt/console/cmd/impl/ListCommand.scala
@@ -4,11 +4,13 @@ package com.maxrt.console.cmd.impl
 import com.maxrt.console.cmd.Command
 
 class ListCommand extends Command {
+  private val tables = List("album", "author", "genre", "musical_label", "saved_song", "song", "user")
+
   def check(cmdName: String): Boolean = cmdName == "list"
 
   def run(args: List[String]): Unit = {
     if (args.length == 1) {
-//      Table.tables.foreach(println)
+      tables.foreach(println)
     } else {
       println("Usage: list")
     }
