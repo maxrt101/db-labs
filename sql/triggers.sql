8bc29b673d1ed6c12991ae83a3fdf90cde12ffd8
Maksym Tkachuk <s2m4f6@gmail.com>
2021-11-25 04:09:22 +0200

    Added unique checks & fixes for some triggers

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -7,6 +7,10 @@ BEFORE INSERT
 ON discipline 
 FOR EACH ROW
 BEGIN
+  IF (new.id IN (SELECT id FROM discipline)) THEN
+    SIGNAL SQLSTATE "45000"
+    SET MESSAGE_TEXT = "Insert failed for table discipline: This id is already present";
+  END IF;
   IF (new.form NOT IN (SELECT id FROM acceptance_form)) THEN
     SIGNAL SQLSTATE "45000"
     SET MESSAGE_TEXT = "Insert failed for table discipline: No such id in table acceptance_form";
@@ -23,6 +27,10 @@ BEFORE UPDATE
 ON discipline
 FOR EACH ROW
 BEGIN
+  IF (new.id IN (SELECT id FROM discipline)) THEN
+    SIGNAL SQLSTATE "45000"
+    SET MESSAGE_TEXT = "Update failed for table discipline: This id is already present";
+  END IF;
   IF (new.form NOT IN (SELECT id FROM acceptance_form)) THEN
     SIGNAL SQLSTATE "45000"
     SET MESSAGE_TEXT = "Update failed for table discipline: No such id in table acceptance_form";
@@ -39,6 +47,10 @@ BEFORE INSERT
 ON employee
 FOR EACH ROW
 BEGIN
+  IF (new.id IN (SELECT id FROM employee)) THEN
+    SIGNAL SQLSTATE "45000"
+    SET MESSAGE_TEXT = "Insert failed for table employee: This id is already present";
+  END IF;
   IF (new.position NOT IN (SELECT id FROM position)) THEN
     SIGNAL SQLSTATE "45000"
     SET MESSAGE_TEXT = "Insert failed for table employee: No such id in table position";
@@ -63,25 +75,29 @@ DROP TRIGGER IF EXISTS employee_update;
 
 DELIMITER $$
 CREATE TRIGGER employee_update
-BEFORE INSERT
+BEFORE UPDATE
 ON employee
 FOR EACH ROW
 BEGIN
+  IF (new.id IN (SELECT id FROM employee)) THEN
+    SIGNAL SQLSTATE "45000"
+    SET MESSAGE_TEXT = "Update failed for table employee: This id is already present";
+  END IF;
   IF (new.position NOT IN (SELECT id FROM position)) THEN
     SIGNAL SQLSTATE "45000"
-    SET MESSAGE_TEXT = "Insert failed for table employee: No such id in table position";
+    SET MESSAGE_TEXT = "Update failed for table employee: No such id in table position";
   END IF;
   IF (new.status NOT IN (SELECT id FROM academic_status)) THEN
     SIGNAL SQLSTATE "45000"
-    SET MESSAGE_TEXT = "Insert failed for table employee: No such id in table academic_status";
+    SET MESSAGE_TEXT = "Update failed for table employee: No such id in table academic_status";
   END IF;
   IF (new.degree NOT IN (SELECT id FROM degree)) THEN
     SIGNAL SQLSTATE "45000"
-    SET MESSAGE_TEXT = "Insert failed for table employee: No such id in table degree";
+    SET MESSAGE_TEXT = "Update failed for table employee: No such id in table degree";
   END IF;
   IF (new.expirience > (YEAR(NOW()) - YEAR(new.birthday) + 15)) THEN
     SIGNAL SQLSTATE "45000"
-    SET MESSAGE_TEXT = "Insert failed for table employee: Expirience is not possible`";
+    SET MESSAGE_TEXT = "Update failed for table employee: Expirience is not possible`";
   END IF;
 END $$
 DELIMITER ;
@@ -97,7 +113,55 @@ FOR EACH ROW
 BEGIN
   IF (old.id IN (SELECT id FROM employee_discipline)) THEN
     SIGNAL SQLSTATE "45000"
-    SET MESSAGE_TEXT = "Update failed for table employee: No such id in table employee_discipline";
+    SET MESSAGE_TEXT = "Delete failed for table employee: No such id in table employee_discipline";
+  END IF;
+END $$
+DELIMITER ;
+
+
+DROP TRIGGER IF EXISTS position_insert;
+
+DELIMITER $$
+CREATE TRIGGER position_insert
+BEFORE INSERT
+ON position
+FOR EACH ROW
+BEGIN
+  IF (new.id IN (SELECT id FROM position)) THEN
+    SIGNAL SQLSTATE "45000"
+    SET MESSAGE_TEXT = "Insert failed for table position: This id is already present";
+  END IF;
+END $$
+DELIMITER ;
+
+
+DROP TRIGGER IF EXISTS status_insert;
+
+DELIMITER $$
+CREATE TRIGGER status_insert
+BEFORE INSERT
+ON academic_status
+FOR EACH ROW
+BEGIN
+  IF (new.id IN (SELECT id FROM academic_status)) THEN
+    SIGNAL SQLSTATE "45000"
+    SET MESSAGE_TEXT = "Insert failed for table academic_status: This id is already present";
+  END IF;
+END $$
+DELIMITER ;
+
+
+DROP TRIGGER IF EXISTS degree_insert;
+
+DELIMITER $$
+CREATE TRIGGER degree_insert
+BEFORE INSERT
+ON degree
+FOR EACH ROW
+BEGIN
+  IF (new.id IN (SELECT id FROM degree)) THEN
+    SIGNAL SQLSTATE "45000"
+    SET MESSAGE_TEXT = "Insert failed for table degree: This id is already present";
   END IF;
 END $$
 DELIMITER ;
@@ -111,6 +175,10 @@ BEFORE UPDATE
 ON position
 FOR EACH ROW
 BEGIN
+  IF (new.id IN (SELECT id FROM position)) THEN
+    SIGNAL SQLSTATE "45000"
+    SET MESSAGE_TEXT = "Update failed for table position: This id is already present";
+  END IF;
   IF (old.id IN (SELECT position FROM employee)) THEN
     SIGNAL SQLSTATE "45000"
     SET MESSAGE_TEXT = "Update failed for table position: There are rows in employee that are referencing this row ";
@@ -127,6 +195,10 @@ BEFORE UPDATE
 ON academic_status
 FOR EACH ROW
 BEGIN
+  IF (new.id IN (SELECT id FROM academic_status)) THEN
+    SIGNAL SQLSTATE "45000"
+    SET MESSAGE_TEXT = "Update failed for table academic_status: This id is already present";
+  END IF;
   IF (old.id IN (SELECT status FROM employee)) THEN
     SIGNAL SQLSTATE "45000"
     SET MESSAGE_TEXT = "Update failed for table status: There are rows in employee that are referencing this row ";
@@ -143,6 +215,10 @@ BEFORE UPDATE
 ON degree
 FOR EACH ROW
 BEGIN
+  IF (new.id IN (SELECT id FROM degree)) THEN
+    SIGNAL SQLSTATE "45000"
+    SET MESSAGE_TEXT = "Update failed for table degree: This id is already present";
+  END IF;
   IF (old.id IN (SELECT degree FROM employee)) THEN
     SIGNAL SQLSTATE "45000"
     SET MESSAGE_TEXT = "Update failed for table degree: There are rows in employee that are referencing this row ";
@@ -199,6 +275,38 @@ END $$
 DELIMITER ;
 
 
+DROP TRIGGER IF EXISTS form_insert;
+
+DELIMITER $$
+CREATE TRIGGER form_insert
+BEFORE INSERT
+ON acceptance_form
+FOR EACH ROW
+BEGIN
+  IF (new.id IN (SELECT id FROM acceptance_form)) THEN
+    SIGNAL SQLSTATE "45000"
+    SET MESSAGE_TEXT = "Insert failed for table acceptance_form: This id is already present";
+  END IF;
+END $$
+DELIMITER ;
+
+
+DROP TRIGGER IF EXISTS form_update;
+
+DELIMITER $$
+CREATE TRIGGER form_update
+BEFORE INSERT
+ON acceptance_form
+FOR EACH ROW
+BEGIN
+  IF (new.id IN (SELECT id FROM acceptance_form)) THEN
+    SIGNAL SQLSTATE "45000"
+    SET MESSAGE_TEXT = "Update failed for table acceptance_form: This id is already present";
+  END IF;
+END $$
+DELIMITER ;
+
+
 DROP TRIGGER IF EXISTS form_delete;
 
 DELIMITER $$
@@ -223,6 +331,10 @@ BEFORE INSERT
 ON employee_discipline
 FOR EACH ROW
 BEGIN
+  IF (new.id IN (SELECT id FROM employee_discipline)) THEN
+    SIGNAL SQLSTATE "45000"
+    SET MESSAGE_TEXT = "Insert failed for table employee_discipline: This id is already present";
+  END IF;
   IF (new.employee NOT IN (SELECT id FROM employee)) THEN
     SIGNAL SQLSTATE "45000"
     SET MESSAGE_TEXT = "Insert failed for table position: There are bo such id in table employee";
@@ -243,13 +355,17 @@ BEFORE UPDATE
 ON employee_discipline
 FOR EACH ROW
 BEGIN
+  IF (new.id IN (SELECT id FROM employee_discipline)) THEN
+    SIGNAL SQLSTATE "45000"
+    SET MESSAGE_TEXT = "Update failed for table employee_discipline: This id is already present";
+  END IF;
   IF (new.employee NOT IN (SELECT id FROM employee)) THEN
     SIGNAL SQLSTATE "45000"
-    SET MESSAGE_TEXT = "Insert failed for table position: There are bo such id in table employee";
+    SET MESSAGE_TEXT = "Update failed for table position: There are bo such id in table employee";
   END IF;
   IF (new.discipline NOT IN (SELECT id FROM discipline)) THEN
     SIGNAL SQLSTATE "45000"
-    SET MESSAGE_TEXT = "Insert failed for table position: There are bo such id in table discipline";
+    SET MESSAGE_TEXT = "Update failed for table position: There are bo such id in table discipline";
   END IF;
 END $$
 DELIMITER ;
